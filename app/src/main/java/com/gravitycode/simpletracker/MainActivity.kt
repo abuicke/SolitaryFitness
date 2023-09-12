b8deb931c6934460b51f6ffc61282f70c3fe34dd
abuicke <buickea@gmail.com>
2023-09-12 22:18:37 +0100

    workouts appear styled in cards

--- app/src/main/java/com/gravitycode/simpletracker/MainActivity.kt
+++ app/src/main/java/com/gravitycode/simpletracker/MainActivity.kt
@@ -3,15 +3,21 @@ package com.gravitycode.simpletracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gravitycode.simpletracker.ui.theme.SimpleTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +27,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleTrackerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     WorkoutsList()
                 }
             }
@@ -31,14 +40,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WorkoutsList() {
- LazyList(listItems = Workouts.DEFAULT_WORKOUTS)
+    LazyList(listItems = Workouts.DEFAULT_WORKOUTS)
 }
 
 @Composable
 fun <E> LazyList(listItems: List<E>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier) {
+    LazyColumn(
+        modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         items(listItems) { listItem ->
-            Text(text = listItem.toString())
+            Card(
+                modifier = Modifier
+                    .padding(12.dp, 6.dp, 12.dp, 6.dp)
+                    .fillMaxSize()
+            ) {
+                Text(
+                    modifier = Modifier.padding(12.dp),
+                    text = listItem.toString(),
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
