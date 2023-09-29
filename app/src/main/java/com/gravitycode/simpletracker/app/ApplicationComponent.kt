5da56c1c0e7e638a6b316faf983238720bd2b987
abuicke <buickea@gmail.com>
2023-09-29 06:55:18 +0100

    more TODOs

--- app/src/main/java/com/gravitycode/simpletracker/app/ApplicationComponent.kt
+++ app/src/main/java/com/gravitycode/simpletracker/app/ApplicationComponent.kt
@@ -6,5 +6,11 @@ import dagger.Component
 @Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
 
+    /**
+     * TODO: Can this function be called anything? Are all functions exposed by components automatically injection sites?
+     * TODO: Should this injection be happening here? Shouldn't it be happening in
+     * [com.gravitycode.simpletracker.workout_list.WorkoutListComponent]? Can I share [ApplicationComponent]
+     * as a parent to all the feature components, e.g. [com.gravitycode.simpletracker.workout_list.WorkoutListComponent]?
+     * */
     fun inject(activity: WorkoutListActivity)
 }
