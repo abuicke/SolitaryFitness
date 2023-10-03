e5e2e0d32f09f3e0cd17aa0f9cde91fc2807a4ea
abuicke <buickea@gmail.com>
2023-10-03 22:07:00 +0100

    added back UI

--- app/src/main/java/com/gravitycode/simpletracker/workout_list/WorkoutListActivity.kt
+++ app/src/main/java/com/gravitycode/simpletracker/workout_list/WorkoutListActivity.kt
@@ -3,14 +3,21 @@ package com.gravitycode.simpletracker.workout_list
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.lifecycle.lifecycleScope
 import com.gravitycode.simpletracker.app.SimpleTrackerApp
+import com.gravitycode.simpletracker.app.ui.SimpleTrackerTheme
 import com.gravitycode.simpletracker.workout_list.data.WorkoutHistory
 import com.gravitycode.simpletracker.workout_list.data.WorkoutHistoryRepo
 import com.gravitycode.simpletracker.workout_list.data.WorkoutHistoryRepoImpl
 import com.gravitycode.simpletracker.workout_list.domain.WorkoutListViewModel
+import com.gravitycode.simpletracker.workout_list.presentation.WorkoutListScreen
 import com.gravitycode.simpletracker.workout_list.util.Workout
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -29,17 +36,17 @@ class WorkoutListActivity : ComponentActivity() {
         workoutListComponent = appComponent.workoutListComponent().create()
         workoutListComponent.inject(this)
 
-//        setContent {
-//            SimpleTrackerTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    WorkoutListScreen(workoutListViewModel)
-//                }
-//            }
-//        }
+        setContent {
+            SimpleTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WorkoutListScreen(workoutListViewModel)
+                }
+            }
+        }
     }
 
     fun workoutHistoryDataStoreTest(dataStore: DataStore<Preferences>) {
