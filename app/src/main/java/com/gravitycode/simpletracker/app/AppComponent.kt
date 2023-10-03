93943351cc42017ca75fe1a26a9da83d5fa3e873
abuicke <buickea@gmail.com>
2023-10-03 10:30:07 +0100

    added doc to AppComponent

--- app/src/main/java/com/gravitycode/simpletracker/app/AppComponent.kt
+++ app/src/main/java/com/gravitycode/simpletracker/app/AppComponent.kt
@@ -5,6 +5,20 @@ import com.gravitycode.simpletracker.workout_list.WorkoutListComponent
 import dagger.BindsInstance
 import dagger.Component
 
+/**
+ * Test Dagger isn't producing different instances when it shouldn't and different instances
+ * when it should like this (alternatively inject the dependencies into the relevant activity
+ * and test in the same way with `assert`):
+ *
+ *      val applicationGraph: ApplicationGraph = DaggerApplicationGraph.create()
+ *
+ *      val userRepository: UserRepository = applicationGraph.repository()
+ *      val userRepository2: UserRepository = applicationGraph.repository()
+ *
+ *      assert(userRepository != userRepository2)
+ *
+ * @see [https://developer.android.com/training/dependency-injection/dagger-basics#dagger-components]
+ * */
 @ApplicationScope
 @Component(modules = [AppModule::class])
 interface AppComponent {
