85b182e5df95477b65bc5c8e2d8cc06fb8d0360a
abuicke <buickea@gmail.com>
2023-10-16 17:29:37 +0100

    new background colour

--- app/src/main/java/com/gravitycode/simpletracker/workout_list/presentation/WorkoutListScreen.kt
+++ app/src/main/java/com/gravitycode/simpletracker/workout_list/presentation/WorkoutListScreen.kt
@@ -2,6 +2,8 @@ package com.gravitycode.simpletracker.workout_list.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -13,11 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -41,7 +43,9 @@ import com.gravitycode.simpletracker.workout_list.util.Workout
  * [https://developer.android.com/jetpack/compose/state#restore-ui-state]
  * */
 
-private val ADD_REPS_BG = Color(81, 207, 135)
+private val TITLE_AND_COUNT_BTN_COLOUR = Color(140, 177, 189)
+private val ADD_REPS_BTN_COLOUR = Color(81, 207, 135)
+private val ADD_REPS_BTN_COLOUR_PRESSED = Color(81, 207, 135)
 
 @Composable
 @Preview(showSystemUi = true)//, widthDp = 250)
@@ -105,7 +109,7 @@ fun TitleAndCount(
     count: Int
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier.background(TITLE_AND_COUNT_BTN_COLOUR),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -130,12 +134,20 @@ fun TitleAndCount(
     }
 }
 
+/**
+ * TODO: Buttons should change to a lighter colour while pressed.
+ * */
 @Composable
 fun AddRepsButtonRow(modifier: Modifier = Modifier, onClickReps: (Int) -> Unit) {
     Row(
-        modifier.background(ADD_REPS_BG),
+        modifier.background(ADD_REPS_BTN_COLOUR),
         verticalAlignment = Alignment.CenterVertically
     ) {
+
+        val interactionSource = remember { MutableInteractionSource() }
+        val isPressed by interactionSource.collectIsPressedAsState()
+        val pressedColour =
+
         Text(
             modifier = Modifier
                 .weight(1f)
