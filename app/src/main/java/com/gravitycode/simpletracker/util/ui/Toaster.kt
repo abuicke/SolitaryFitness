8061bf8e362c9a3b3973f108883094619f396f3c
abuicke <buickea@gmail.com>
2023-10-24 09:50:42 +0200

    added to Toaster TODO

--- app/src/main/java/com/gravitycode/simpletracker/util/ui/Toaster.kt
+++ app/src/main/java/com/gravitycode/simpletracker/util/ui/Toaster.kt
@@ -8,6 +8,8 @@ import com.gravitycode.simpletracker.app.ApplicationScope
 /**
  * TODO: This should be implementing an interface for testing, but then I lose the nice, neat
  *  syntax of overriding the invoke operator. What's done in the case of use case testing?
+ *  An interface plus a factory would solve this problem?
+ *  Can also just mark the toaster parameter as nullable and not pass it in during testing.
  * */
 @ApplicationScope
 class Toaster(private val context: Context) {
