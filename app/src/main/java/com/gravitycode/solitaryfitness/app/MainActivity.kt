2ab2e93e84380a9d9e52086f6a14bc6b7321c553
abuicke <buickea@gmail.com>
2023-11-15 12:45:15 +0000

    cleaned up MainActivity imports

--- app/src/main/java/com/gravitycode/solitaryfitness/app/MainActivity.kt
+++ app/src/main/java/com/gravitycode/solitaryfitness/app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.gravitycode.solitaryfitness.app
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -12,19 +11,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.firestore.FirebaseFirestore
 import com.gravitycode.solitaryfitness.app.ui.SolitaryFitnessTheme
 import com.gravitycode.solitaryfitness.auth.Authenticator
-import com.gravitycode.solitaryfitness.auth.User
-import com.gravitycode.solitaryfitness.track_reps.data.TestFirestoreWorkoutHistoryRepo
-import com.gravitycode.solitaryfitness.track_reps.data.WorkoutHistoryRepo
-import com.gravitycode.solitaryfitness.track_reps.domain.WorkoutLog
 import com.gravitycode.solitaryfitness.track_reps.presentation.TrackRepsScreen
 import com.gravitycode.solitaryfitness.track_reps.presentation.TrackRepsViewModel
 import com.gravitycode.solitaryfitness.util.debugError
 import com.gravitycode.solitaryfitness.util.ui.Toaster
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 import javax.inject.Inject
 
 /**
