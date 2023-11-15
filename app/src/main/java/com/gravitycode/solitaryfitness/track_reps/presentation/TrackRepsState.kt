7fc4e668a68d0b8246b79ddfdeb8ebad8b9afc65
abuicke <buickea@gmail.com>
2023-11-15 12:43:29 +0000

    removed commented our code from TrackRepsState

--- app/src/main/java/com/gravitycode/solitaryfitness/track_reps/presentation/TrackRepsState.kt
+++ app/src/main/java/com/gravitycode/solitaryfitness/track_reps/presentation/TrackRepsState.kt
@@ -7,24 +7,4 @@ import java.time.LocalDate
 class TrackRepsState(
     val date: LocalDate,
     val log: WorkoutLog = WorkoutLog()
-) {
-
-    /**
-     * TODO: I thought the point of the data class was that it generates exactly these functions?
-     *  Need to verify and remove.
-     * */
-
-//    override fun equals(other: Any?): Boolean {
-//        if (this === other) return true
-//
-//        return other is TrackRepsState &&
-//                date == other.date &&
-//                log == other.log
-//    }
-//
-//    override fun hashCode(): Int {
-//        var result = date.hashCode()
-//        result = 31 * result + log.hashCode()
-//        return result
-//    }
-}
+)
