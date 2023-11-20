9411add13a9acb4a9b3d736eafd3e088279cc3d4
abuicke <buickea@gmail.com>
2023-11-20 07:02:13 +0000

    upgraded activity-compose, added Coil dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,13 +60,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+
+    // Coil - Image Loading
+    implementation("io.coil-kt:coil:2.5.0")
 
     // Multi Dex
     implementation("androidx.multidex:multidex:2.0.1")
