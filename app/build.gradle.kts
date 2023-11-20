3a83aee2d808510a0332fd48a66223220ef41463
abuicke <buickea@gmail.com>
2023-11-20 07:03:38 +0000

    upgraded android core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,7 @@ android {
 
 dependencies {
 
-    // noinspection GradleDependency
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Compose
