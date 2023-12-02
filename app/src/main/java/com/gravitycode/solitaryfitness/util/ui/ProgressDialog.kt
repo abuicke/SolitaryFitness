619d6bc0e820390390639add02e9cdeaa9c1ff0e
abuicke <buickea@gmail.com>
2023-12-02 18:26:14 +0000

    set up skeleton for implementation of ProgressDialog

---
+++ app/src/main/java/com/gravitycode/solitaryfitness/util/ui/ProgressDialog.kt
@@ -0,0 +1,21 @@
+package com.gravitycode.solitaryfitness.util.ui
+
+import android.app.AlertDialog
+import android.content.Context
+import android.util.Log
+
+class ProgressDialog(context: Context) : AlertDialog(context) {
+
+    override fun show() {
+        super.show()
+        /*
+        * TODO: Start recording time here. If the minimum necessary display time has not been reached by the
+        *  time dismiss() is called then wait for the period of the delta.
+        * */
+    }
+
+    override fun dismiss() {
+        super.dismiss()
+
+    }
+}
