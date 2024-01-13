cbfdfb8ed6ae015c50ee5f93870e9926befda443
abuicke <buickea@gmail.com>
2024-01-13 20:06:26 +0000

    added null check where NullPointerException is occurring

--- app/src/main/java/com/gravitycode/solitaryfitness/util/data/Utils.kt
+++ app/src/main/java/com/gravitycode/solitaryfitness/util/data/Utils.kt
@@ -8,7 +8,10 @@ import androidx.datastore.preferences.preferencesDataStoreFile
 
 fun megabytesToBytes(megabytes: Number) = megabytes.toLong() * 1024 * 1024
 
+@Suppress("RedundantRequireNotNullCall")
 fun createPreferencesStoreFromFile(context: Context, name: String): DataStore<Preferences> {
+    // Shouldn't be necessary, but seems to be causing an issue.
+    checkNotNull(context)
     return PreferenceDataStoreFactory.create {
         context.preferencesDataStoreFile(name)
     }
