c7d300d27ad4d9c20d4b61035efef4c75e49fa52
abuicke <buickea@gmail.com>
2024-03-26 15:48:54 +0000

    now working with remote caimito library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 
 dependencies {
 
-    implementation(project(":caimito"))
+    implementation("dev.gravitycode.caimito:caimito:2.0.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -68,7 +68,7 @@ dependencies {
     // Compose
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,11 +11,12 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
+        maven(url = "https://s01.oss.sonatype.org/content/repositories/releases/")
     }
 }
 
 rootProject.name = "SolitaryFitness"
 include(":app")
 
-include(":caimito")
-project(":caimito").projectDir = File("..\\caimito\\caimito")
+// include(":caimito")
+// project(":caimito").projectDir = File("..\\caimito\\caimito")
